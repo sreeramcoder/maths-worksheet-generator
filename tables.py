1ac7c8916acdd3c129218bb3d22f86efd046fc92
sreeramvenkat <sreeramvenkat@yahoo.com>
2023-12-10 12:19:10 +0530

    initial

--- tables.py
+++ tables.py
@@ -11,14 +11,14 @@ class PDF(FPDF):
         
 
 def addition():
-    a = random.randint(0, 10)
-    b = random.randint(0, 10 - a)
+    a = random.randint(1, 9)
+    b = random.randint(1, 10 - a)
     op = '+'
     return f'{a} {op} {b} = _____'
 
 def subtraction():
     a = random.randint(1, 10)
-    b = random.randint(0, a)
+    b = random.randint(1, a)
     
     op = '-'
     return f'{a} {op} {b} = _____'
@@ -28,15 +28,37 @@ def multiplication():
     op = 'x'
     return f'{a} {op} {b} = _____'
 
+# 4 + 4 + 4 + 4 + 4 = _______
+def mult_1():
+    a, b = random.randint(1, 8), random.randint(2, 8)
+    op = '+'
+    
+    return f'{a} {op} '*(b-1) + f'{a} = _____'
+
+def tens_1():
+    ntens = random.randint(2,5)
+
+    s = list()
+    for _ in range(ntens):
+        n = random.randint(1,9)
+        m = 10 - n
+        s.append(f'{m}')
+        s.append(f'{n}')
+
+    #random.shuffle(s)
+    return '+'.join(s) + "= _____"
+    
+        
+     
 def generate_question():
-    types = [addition, subtraction, multiplication]
+    types = [addition, subtraction, multiplication, mult_1, tens_1]
 
     choice = types[random.randint(0, len(types)-1)]
     
     return choice()
 
        
-def generate_questions(num_questions=500):
+def generate_questions(num_questions=756):
     questions = []
 
     for i in range(num_questions):
@@ -49,15 +71,15 @@ def create_pdf(questions):
     pdf.add_page()
     pdf.set_font("Helvetica", size=12)
     pdf.set_auto_page_break(auto=True, margin=15)
-    column_width = 60  # Width of each column
+    column_width = 90  # Width of each column
     column = 0  # Initialize column count
     for question in questions:
         x_position = 10 + column * column_width
         pdf.set_xy(x_position, pdf.get_y())
         pdf.cell(column_width - 10, 10, question)  # Adjusted width to account for the line of separation
-        if column < 2:
+        if column < 3:
             pdf.line(x_position + column_width - 10, pdf.get_y() - 10, x_position + column_width - 10, pdf.get_y())  # Draw line of separation
-        column = (column + 1) % 3  # Increment column count, and reset to 0 if it reaches 3
+        column = (column + 1) % 2  # Increment column count, and reset to 0 if it reaches 3
         if column == 0:
             pdf.ln(10)  # Move to the next line after every third column
 
